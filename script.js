5d3b2d176b0e1082f48614956948c085b682d85f
Nicolack <105887686+Doggoipod@users.noreply.github.com>
2024-08-28 08:05:02 -0300

    sg

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ const alternativas = [
         enunciado: "No âmbito social, você prefere:",
         alternativa: [
             {
-                texto: "Alternativa 1",
-                afirmaçao: "Afirmação 1",
+                texto: "Você terá uma vida feliz e com paz, onde todos os seus sonhos se realizam, porém terá mais um ano de vida",
+                afirmaçao: "Parabéns!!! Deixou todos os que você ama para trás por ganância",
             },
             {
-                texto: "Alternativa 2",
-                afirmaçao: "Afirmação 2",
+                texto: "Você terá uma vida longa, mas viverá sem alcançar a sua verdadeira felicidade",
+                afirmaçao: "Parabéns!!! Você terá uma vida infeliz só por conta do seu medo bobo de morrer :)",
             },
         ]
         
@@ -24,12 +24,12 @@ const alternativas = [
         enunciado: "No âmbito ambiental, você prefere:",
         alternativa: [
             {
-                texto: "Alternativa 3",
-                afirmaçao: "Afirmação 3",
+                texto: "Você pode optar por converter todas as fontes de energia do mundo em renováveis, garantindo um futuro sustentável, mas isso exigirá que vastas áreas naturais sejam sacrificadas para construir as infraestruturas necessárias ",
+                afirmaçao: "Como se sente sabendo que vários animais morreram por sua culpa?",
             },
             {
-                texto: "Alternativa 4",
-                afirmaçao: "Afirmação 4",
+                texto: "Você pode preservar todos os ecossistemas intocados, mas o mundo continuará dependente de fontes de energia poluentes, acelerando os impactos das mudanças climáticas.",
+                afirmaçao: "Parabéns!!! Você estragou o meio ambiente por conta de alguns ecossistemas inúteis",
             },
         ]
         
@@ -39,8 +39,8 @@ const alternativas = [
         enunciado: "No âmbito tecnológico, você prefere:",
         alternativa: [
             {
-                texto: "Alternativa 5",
-                afirmaçao: "Afirmação 5",
+                texto: "Você pode implantar um chip em seu cérebro que lhe concederá acesso instantâneo a todo o conhecimento da humanidade, permitindo que você se torne a pessoa mais inteligente e informada do planeta, mas perderá gradualmente a capacidade de sonhar, imaginar e criar algo novo por conta própria. ",
+                afirmaçao: "Parabéns inteligentão!!! Está com todos os conhecimentos do mundo ao seu dispor ",
             },
             {
                 texto: "Alternativa 6",
@@ -51,24 +51,22 @@ const alternativas = [
     },
 ]
 
-let atual = 0; 
+let atual = 0;
 let perguntaAtual;
 let historiaFinal = "";
 
-function mostrarPerguntas () { 
+function mostraPerguntas (){
     if(atual>= alternativas.length){
         mostraResultado();
         return;
     }
-    perguntaAtual = alternativas [atual];
+    perguntaAtual = alternativas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
-    caixaAlternativas.textContent = "";
-    mostrarAlternativas();
+    caixaAlternativas.textContent= "";
+    mostraAlternativas();
 }
 
-
-function mostrarAlternativas(){
-   
+function mostraAlternativas(){
     for (const opcao of perguntaAtual.alternativa) {
         const botaoAlternativa = document.createElement('button');
         botaoAlternativa.textContent = opcao.texto;
@@ -78,10 +76,10 @@ function mostrarAlternativas(){
 }
 
 function respostaSelecionada(opcao){
-    const afirmacoes = opcao.afirmaçao;
-    historiaFinal += afirmacoes = " ";
+    const afirmacoes = opcao.alternativas;
+    historiaFinal += afirmacoes + " ";
     atual++;
-    mostrarPerguntas();
+    mostraPerguntas();
 }
 
 function mostraResultado (){
@@ -90,4 +88,4 @@ function mostraResultado (){
     caixaAlternativas.textContent = "";
 }
 
-mostrarPerguntas();
+mostraPerguntas();
